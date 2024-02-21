593fe9cf2d50cac873ce536037bc6b70dbcd5362
DanielloJad <jelolagman@gmail.com>
2024-02-21 08:09:10 +0800

    updated App.php

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -39,5 +39,14 @@ class App
         $this->params = $url ? array_values($url) : [];
         call_user_func_array([$this->controller, $this->method, $this->params]);
     }
+
+    private function splitUrl()
+    {
+        if (isset($_GET['url']))
+        {
+            $url explode('/', trim($_GET['url'], '/'));
+            return $url;
+        }
+    }
 }
    
