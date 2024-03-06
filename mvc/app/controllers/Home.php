d01811aa13fdac958d6b02676a927a8bfde19ac9
Laurence <johnlaurence.gaviola@gmail.com>
2024-03-06 09:19:40 +0800

    created database and connection

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -4,6 +4,10 @@ class Home extends Controller
 {
     public function Index()
     {
+        $model = new Model();
+        $data = $model->findAll();
+        show($data);
+
         $this->view('home');
     }
 }
---
+++ mvc/app/core/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database
+{
+    public function connect()
+    {
+        //create users table with field names: ID, firstname, lastname, email, password
+
+        $string = "mysql:host=localhost;dbname=mvc_BSIS3G";
+        $con = new PDO($string, 'root', '');
+        return $con;
+    }
+
+    public function query($query, $data=[])
+    {
+        $con = $this->connect();
+        $stm = $con->prepare($query);
+        $check = $stm->execute($data);
+
+        if($check)
+        {
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+
+            if(is_array($result) && count($result) > 0)
+            {
+                return $result;
+            }
+        }
+
+        return false;
+    }
+}
